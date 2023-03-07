afeab80ec645006845a960af75fd6620aa1bf52c
[kim-dahun] <ekgnsl2002@gmail.com>
2023-03-07 18:09:57 +0900

    20230307-03
    ver3

--- lab_java_study/Java04_Array/src/edu/java/array04/ArrayMain04.java
+++ lab_java_study/Java04_Array/src/edu/java/array04/ArrayMain04.java
@@ -23,30 +23,30 @@ public class ArrayMain04 {
 
 		for (int i = 0; i < score.length; i++) {
 			int sc = ran.nextInt(10);
-			score[i] = sc;
-			vsscore[i] = sc;
-			for (int j = 0; j < i; j++) {
-				if (score[i] == vsscore[j]) {
-					score[i] = 11;
-					i--;
+			score[i] = sc; 
+			vsscore[i] = sc; // 같은 인덱스에 원소를 저장
+			for (int j = 0; j < i; j++) { // i 원소 저장 갯수만큼 j 반복
+				if (score[i] == vsscore[j]) { // i로부터 받은 원소와 j에 지금까지 입력된 원소 대조
+					score[i] = 11; // score 배열에 들어가면 안되는 수 중 하나 지정
+					i--; // i 재실행을 위해 횟수 차감
 				}
 			}
-			sum = sum + sc;
+			sum = sum + sc; // i 반복문 1회 실행 시마다 sum 함수에 배열원소값 가산
 		}
 
 		for (int x : score) {
-			System.out.print(x + " ");
+			System.out.print(x + " "); // for-each 항목으로 score 배열 나열
 
 		}
 
 		System.out.println();
-		System.out.println("정렬을 원하시면 <정렬> 이라고 입력해주세요.\n");
-		String select = scan.next();
+		System.out.println("정렬을 원하시면 <정렬> 이라고 입력해주세요.");
+		String select = scan.next(); // 글자 String 을 받아들이기 위한 Scanner
 		
 
 		if (select.equals("정렬")) {
-			Arrays.sort(score);
-			for (int x : score) {
+			Arrays.sort(score); // 배열 오름차순으로 정렬
+			for (int x : score) { 
 				System.out.print(x + " ");
 
 			}
@@ -55,9 +55,9 @@ public class ArrayMain04 {
 		}
 
 		System.out.println();
-		System.out.println("배열 원소의 합계 : " + sum);
-		double avg = (double) sum / score.length;
-		System.out.println("배열 원소들의 평균은 " + avg);
+		System.out.println("배열 원소의 합계 : " + sum); // 합계 표시
+		double avg = (double) sum / score.length; // 합계를 (double) 변수로 캐스팅한 후 그 값을 인덱스 길이로 나눔.
+		System.out.println("배열 원소들의 평균은 " + avg); // 평균값 표시
 	}
 
 }
