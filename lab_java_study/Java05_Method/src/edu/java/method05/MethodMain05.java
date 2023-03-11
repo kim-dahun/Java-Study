3d367c0901ed27b271c2e8a44cf20e8e7f36d273
kim-dahun <117079872+kim-dahun@users.noreply.github.com>
2023-03-11 19:04:48 +0900

    20230311-01
    ver01

--- lab_java_study/Java05_Method/src/edu/java/method05/MethodMain05.java
+++ lab_java_study/Java05_Method/src/edu/java/method05/MethodMain05.java
@@ -17,6 +17,45 @@ public class MethodMain05 {
 		// (주의) 가변 길이 argument 는 가장 마지막 parameter 가 되야 한다.
 		// -> (String X, object ... Y)
 		// 가변길이 argument를 갖는 parameter는 오직 1개만 선언할 수 있다.
+		
+		double result = calculate("+");
+		System.out.println(result);	
+		
+		double[] arr = {1.0, 1.1}; // 가변길이 아규먼트일 때는 배열을 전달해도 됨.
+        System.out.println(calculate("+", arr));
+        
+        System.out.println(calculate("*"));
+		
+	
+	
+	}
+	// 문자열 "*" , "+" 를 받아서 모든 args 의 합계 혹은 곱셈을 반환
+	// 문자열 "*","+" 이외 문자열은 모두 무시;
+	// 문자열 "*" 상황에서 숫자 입력 없을 시 1 출력
+	// 문자열 "+" 상황에서 숫자 입력 없을 시 0 출력
+	
+	public static double calculate(String op, double ...args) {
+		double result = 0.0;
+		
+		switch (op) {
+		case "+":
+			for (double x : args) {
+				result += x;
+			}
+			break;
+		case "*":
+			result = 1.0;
+			for ( double x : args) {
+				result *= x; // result = result * x;
+			}
+			break;
+			
+		default:
+		}
+		
+		return result;
+		
+		
 	}
 
 }
---
+++ lab_java_study/sample_pj/src/sample/work2/Methodre01.java
@@ -0,0 +1,30 @@
+package sample.work2;
+
+public class Methodre01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = round(3,5);
+		System.out.println(a);
+		int num = 5;
+		System.out.println(num*5);
+	}
+	// 원하는 숫자의 n제곱 확인
+	public static int round(int roundnum, int number){
+		
+		int result = number;
+		
+		
+		
+		if(roundnum < 2 ) {
+			return result;
+		} else {
+			
+			return result*=round(roundnum-1,number);
+			// rn =3 => 5*roundnum2{5}*roundnum1{5}
+		}
+		
+			
+		
+	}
+}
