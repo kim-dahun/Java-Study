76f29b8544a08baa88b7ffeba531c20e001a603c
kim-dahun <117079872+kim-dahun@users.noreply.github.com>
2023-03-31 00:42:49 +0900

    20230331-01

---
+++ lab_java_study/sample_pj/src/test/TestSol.java
@@ -0,0 +1,22 @@
+package test;
+
+import java.util.Arrays;
+
+public class TestSol {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		long answer = 0;
+
+		String numbers = "";
+		
+		String n = numbers.replace("one", "1");
+
+		char ch1 = 'a';
+		n.matches(".*[A-Z].*");
+		
+		answer = Long.parseLong(n);
+	}
+
+}
