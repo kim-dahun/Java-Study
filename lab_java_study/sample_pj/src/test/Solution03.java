0c83435178453b3c6f3268dce2434336c1d9d9b0
[kim-dahun] <ekgnsl2002@gmail.com>
2023-04-11 18:57:32 +0900

    20230411-01
    2023041101

--- lab_java_study/sample_pj/src/test/Solution03.java
+++ lab_java_study/sample_pj/src/test/Solution03.java
@@ -2,17 +2,28 @@ package test;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public class Solution03 {
 	public String solution(String polynomial) {
         String answer = "";
         
-       
+        Set<String> set = new HashSet<>();
         
+        String[] players = new String[40];
+        
+        Map<String,Integer> map = new HashMap<>();
+        
+        int add = 0;
+        map.put(players[1],+add);
+        map.keySet();
         List<Integer> ia = new ArrayList<>();
         ia.sort(null);
+        
         
         String[] s1 = polynomial.split(" ");
         Arrays.sort(s1);
